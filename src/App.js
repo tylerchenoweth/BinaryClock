d285584c65b97eedbbbf13fce932e48ffda59441
Tyler <tchen358@gmail.com>
2025-05-26 02:53:14 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useRef, useEffect } from "react";
+
+// function CanvasDrawing() {
+//   const canvasRef = useRef(null);
+
+//   useEffect(() => {
+//     const canvas = canvasRef.current;
+//     const ctx = canvas.getContext("2d");
+
+//     // Circle
+//     ctx.beginPath();
+//     ctx.arc(200, 100, 40, 0, 2 * Math.PI);
+//     ctx.fillStyle = "red";
+//     ctx.fill();
+
+//     // // 🔲 Rectangle
+//     // ctx.fillStyle = "blue";
+//     // ctx.fillRect(50, 50, 100, 80);
+
+    
+
+//     // // Triangle
+//     // ctx.beginPath();
+//     // ctx.moveTo(100, 200);
+//     // ctx.lineTo(150, 300);
+//     // ctx.lineTo(50, 300);
+//     // ctx.closePath();
+//     // ctx.fillStyle = "green";
+//     // ctx.fill();
+
+//     // // Line
+//     // ctx.beginPath();
+//     // ctx.moveTo(20, 20);
+//     // ctx.lineTo(200, 20);
+//     // ctx.strokeStyle = "black";
+//     // ctx.lineWidth = 2;
+//     // ctx.stroke();
+//   }, []);
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ tmp/clock.js
@@ -0,0 +1,64 @@
+
+const d = new Date();
+
+let time = d.getTime();
+
+console.log(d);
+
+let hour = d.getHours();
+let minute = d.getMinutes();
+let second = d.getSeconds();
+
+// let day = d.getDay();
+// let month = d.getMonth();
+// let year = d.getFullYear();
+
+
+
+
+console.log("Breakdown: ")
+
+
+
+function convert_to_binary(decimalNumber) {
+    return decimalNumber.toString(2);
+}
+
+
+const map = new Map();
+
+map.set("hour_tens", convert_to_binary(Math.floor(hour/10)));
+map.set("hour_units", convert_to_binary(Math.floor(hour%10)));
+
+map.set("minute_tens", convert_to_binary(Math.floor(minute/10)));
+map.set("minute_units", convert_to_binary(Math.floor(minute%10)));
+
+map.set("second_tens", convert_to_binary(Math.floor(second/10)));
+map.set("second_units", convert_to_binary(Math.floor(second%10)));
+
+
+console.log(hour, " ", map.get("hour_tens"));
+console.log(hour, " ", map.get("hour_units"));
+console.log(minute, " ", map.get("minute_tens"));
+console.log(minute, " ", map.get("minute_units"));
+console.log(second, " ", map.get("second_tens"));
+console.log(second, " ", map.get("second_units"));
+
+
+
+
+
+
+
+
+// function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// async function demo() {
+//     console.log("Start");
+//     await sleep(1000);
+//     console.log("End after 1 second");
+// }
+  
+// demo()
